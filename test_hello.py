9a25af130f84343fd0319ecfe2e1729daeb3b9e8
Özkan <61411331+ozkanuysal@users.noreply.github.com>
2023-12-08 14:55:19 +0300

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -5,8 +5,8 @@ def test_more_hello():
     assert "SA" == more_hello()
 
 
-def test_2_more_hello():
-    assert "ASé" == more_hello()
+#def test_2_more_hello():
+#    assert "ASé" == more_hello()  -> checked for the github action error log and saw yes it was errror on log.!
 
 
 def test_Bye():
